7bbc1311057a5a097d8ba57738340708cb95fb08
Sercan Timocin <sercantimocin@gmail.com>
2021-12-31 20:17:35 +0300

    main added

--- console/build.gradle.kts
+++ console/build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.10"
+    application
 }
 
 group = "me.sercantimocin"
@@ -20,5 +21,9 @@ tasks.test {
 }
 
 tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "1.6"
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
 }

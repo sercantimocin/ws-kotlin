5b11aba51e218e2ddc999e1850b3f964072cbfaa
Sercan Timocin <sercantimocin@gmail.com>
2022-01-03 23:25:17 +0300

    types

---
+++ console/src/main/kotlin/concepts/Types.kt
@@ -0,0 +1,54 @@
+package concepts
+
+fun main() {
+
+    //All nullable references to a are actually the same object because of the memory optimization
+    // that JVM applies to Integers between -128 and 127.
+    // It doesn't apply to the b references, so they are different objects.
+    referenceCheck()
+
+    println()
+
+    //primitiveTypes()
+
+    println()
+
+
+}
+
+fun referenceCheck() {
+    val a: Int = 100
+    val boxedA: Int? = a
+    val anotherBoxedA: Int? = a
+
+    val b: Int = 10000
+    val boxedB: Int? = b
+    val anotherBoxedB: Int? = b
+
+    println(boxedA === anotherBoxedA) //?
+    println(boxedB === anotherBoxedB) //?
+}
+
+fun primitiveTypes(){
+
+    val one = 1
+    val threeBillion = 3000000000
+    val oneLong = 1L
+    val oneByte: Byte = 1
+    val e = 2.7182818284
+    val x = 2.7182818284f
+    val y=0b00001011
+    val z=0x0F
+
+    println(one::class)
+    println(threeBillion::class)
+    println(oneLong::class)
+    println(e::class)
+    println(x::class)
+    println(y::class)
+    println(z::class)
+}
+
+fun typeCheck(){
+
+}
